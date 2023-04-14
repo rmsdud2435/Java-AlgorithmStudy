8941972ca749b63937714e2566e14607e1352891
gykim <rmsdud2435@gmail.com>
2023-04-14 15:54:24 +0900

    [Mod]dfs Algorithm

--- src/com/algorithm/study/programmers/dfs/여행경로.java
+++ src/com/algorithm/study/programmers/dfs/여행경로.java
@@ -422,3 +422,153 @@ class Solution {
     
 }
 */
+/*
+모법답안
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+import java.util.*;
+
+class Solution {
+    List<Stack<String>> result;
+    String[][] tickets;
+
+    public String[] solution(String[][] tickets) {
+        result = new ArrayList<>();
+        this.tickets = tickets;
+
+        boolean[] visited = new boolean[tickets.length];
+        Stack<String> st = new Stack<>();
+        st.push("ICN");
+
+        dfs(visited, st, 0);
+
+        if (result.size() > 1) {
+            Collections.sort(result, new Comparator<Stack<String>>() {
+                @Override
+                public int compare(Stack<String> o1, Stack<String> o2) {
+                    for (int i = 0; i < o1.size(); i++) {
+                        String s1 = o1.get(i);
+                        String s2 = o2.get(i);
+
+                        if (!s1.equals(s2)) {
+                            return s1.compareTo(s2);
+                        }
+                    }
+
+                    return 0;
+                }
+            });
+        }
+
+        Stack<String> res = result.remove(0);
+        String[] answer = new String[res.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = res.get(i);
+        }
+
+        return answer;
+    }
+
+    public void dfs(boolean[] visited, Stack<String> st, int len) {
+        if (len == tickets.length) {
+            Stack<String> res = new Stack<>();
+            for (String s : st) {
+                res.push(s);
+            }
+
+            result.add(res);
+            return;
+        }
+
+        String arrive = st.peek();
+
+        for (int i = 0; i < tickets.length; i++) {
+            String[] tic = tickets[i];
+
+            if (!visited[i] && arrive.equals(tic[0])) {
+                st.push(tic[1]);
+                visited[i] = true;
+
+                dfs(visited, st, len + 1);
+
+                visited[i] = false;
+                st.pop();
+            }
+        }
+    }
+}
+
+*/
