8c85b1a26ba96b72c357385958b90cc9d7c9203d
gykim <rmsdud2435@gmail.com>
2023-04-19 11:10:55 +0900

    [Mod] graph algorith1

--- src/com/algorithm/study/programmers/graph/가장_먼_노드.java
+++ src/com/algorithm/study/programmers/graph/가장_먼_노드.java
@@ -1,13 +1,28 @@
 class Solution {
     
-    int[] nodeArray;
+    private int[] nodeArray;
+    private int maxCnt;
     
     public int solution(int n, int[][] edge) {
         int answer = 0;
         nodeArray = new int[n];
+        bfs()
         System.out.println(nodeArray);
         
         return answer;
+    }
+    
+    private int bfs(int nodeNum, int[][] edge, int currentCnt){
+        for(int i = 0; i < edge.length; i++){
+            int[] vertex = edge[i];
+            if(vertex[0] == nodeNum){
+                if(checkNode(vertex[1], currentCnt){
+                    
+                }
+            }
+        }
+        
+        return 0;
     }
     
     private void initNodeArray(int[] nodeArray){
